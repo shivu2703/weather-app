7dc287f3b703b819ca3c9bc8502dd4a7a6e509ae
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2023-09-29 19:55:46 +0530

    added new changes

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const request= require('request')
 
 const url='http://api.weatherstack.com/current?access_key=c58c324b7d694f3367fd99985322f556&query=26.449923,80.331871'
 
-request({ url : url }, (error, response)=>{
-    const data=JSON.parse(response.body)
-    console.log(data.location)
+// request({ url : url, json: true }, (error, response)=>{
+//     // const data=JSON.parse(response.body)
+//     console.log(response.body.location)
+//     console.log(response.body.current)
+// })
+
+request({url:url, json:true},(error,response)=>{
+    console.log("It is currently "+ response.body.current.temperature +
+     " degree out. It feels like "+response.body.current.feelslike +" degree out there")
 })
