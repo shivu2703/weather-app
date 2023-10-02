17599cc3d96f670c215d19171811df6fd1f3fb20
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2023-10-03 02:21:55 +0530

    added destructuring and object shorthand property

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const address = process.argv[2]
 if (!address) {
     console.log("Please provide a address!!")
 } else {
-    geocode(address, (error, data) => {
+    geocode(address, (error, {Latitude,Longitude,Location}) => {
         if (error) {
             return console.log(error)
         }
-        forecast(data.Latitude, data.Longitude, (error, forecastData) => {
+        forecast(Latitude, Longitude, (error, forecastData) => {
             if (error) {
                 return console.log(error)
             }
-            console.log(data.Location)
+            console.log(Location)
             console.log(forecastData)
         })
     })
--- utils/geocode.js
+++ utils/geocode.js
@@ -3,16 +3,16 @@ const request=require('request')
 const geocode=(address,callback)=>{
     url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=pk.eyJ1Ijoic2hpdnUyNzAzIiwiYSI6ImNsbjR0Y3MycDA1MjEyanA5Nzh0cmRlZWIifQ.mTz8XDYGPOQNRHw2rzdvnQ&limit=1'
     
-    request({url: url, json: true},(error,response)=>{
+    request({url, json: true},(error,{body})=>{
        if(error){
         callback('Unable to connect to geocoding api service',undefined)
-       }else if(response.body.features.length === 0){
+       }else if(body.features.length === 0){
         callback('Unable to get the location. Try another search ',undefined)
        }else{
          callback(undefined,{
-            Longitude: response.body.features[0].center[0],
-            Latitude: response.body.features[0].center[1],
-            Location: response.body.features[0].place_name
+            Longitude: body.features[0].center[0],
+            Latitude:  body.features[0].center[1],
+            Location:  body.features[0].place_name
          })
        }
 
