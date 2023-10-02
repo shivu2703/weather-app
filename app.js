d26e4142253b52bbec6ba328ada65531e3d8a2ee
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2023-10-02 22:10:38 +0530

    added geocode.js file in utils folder

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const request= require('request')
+const request = require('request')
+const geocode=require('./utils/geocode')
 
-const url='http://api.weatherstack.com/current?access_key=c58c324b7d694f3367fd99985322f556&query=26.449923,80.331871'
 
-// request({ url : url, json: true }, (error, response)=>{
-//     // const data=JSON.parse(response.body)
-//     console.log(response.body.location)
-//     console.log(response.body.current)
-// })
+url='http://api.weatherstack.com/current?access_key=c58c324b7d694f3367fd99985322f556&query=26.449923,80.331871&units=f'
 
 request({url:url, json:true},(error,response)=>{
-    console.log("It is currently "+ response.body.current.temperature +
+    if(error){
+        console.log("Unable to connect to the weather service")
+    }else if(response.body.error){
+        console.log("Unable to find location")
+    }else{
+    console.log(response.body.current.weather_descriptions[0]+". It is currently "+ response.body.current.temperature +
      " degree out. It feels like "+response.body.current.feelslike +" degree out there")
+    }
+})
+
+
+geocode('Delhi',(error,data)=>{
+   console.log('error',error)
+   console.log('data',data)
 })
