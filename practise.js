c2bbe210cc624f5d7df5060ca4314f4663d2d368
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2023-11-28 12:08:08 +0530

    new changes

--- practise.js
+++ practise.js
@@ -1,11 +1,12 @@
 const request = require('request')
 
+
 const url = 'http://api.weatherstack.com/current?access_key=c58c324b7d694f3367fd99985322f556&query=26.449923,80.331871'
 
-request({url:url},(error, response)=>{
-    const data=JSON.parse(response.body)
-    console.log(data.current)
-})
+// request({url:url},(error, response)=>{
+//     const data=JSON.parse(response.body)
+//     console.log(data.current)
+// })
 
 request({url:url, json:true},(error,response)=>{
     console.log(response.body.current)
